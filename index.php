e7c09d4a7e19e1c702fb31df33cae340b815dfe8
Maiteh <maiteh_m@hotmail.com>
2014-11-05 09:54:53 +0100

    db lussen

--- index.php
+++ index.php
@@ -3,4 +3,32 @@
 	echo "hello world";
 
 	echo "You are here" . $_SERVER["REMOTE_ADDR"]; 
+
+$servername = "10.68.255.225";
+$username = "imd";
+$password = "weareimd";
+$dbname = "lesDNS";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT Code, Name FROM Country";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>ID</th><th>Name</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["Code"]."</td><td>".$row["Name"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
 ?>
